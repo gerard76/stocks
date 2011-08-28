d3508f1992257f16351fd4c6b6e43d42965cc6a7
Gerard Leijdekkers <gerard@eet.nu>
2011-08-28 19:22:14 +0200

    Move to MySQL so I can select Quote by year

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.9'
-gem 'sqlite3'
+gem 'mysql2', '~> 0.2.8'
 
 gem 'chronic'
 gem 'haml'
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,6 @@
 class Quote < ActiveRecord::Base
   default_scope :order => "date ASC"
-  scope :symbols, select(:symbol).group(:symbol)
   
+  scope :symbols, select(:symbol).group(:symbol)
+  scope :year, lambda { |year| where("YEAR(date) = #{year}") }
 end
