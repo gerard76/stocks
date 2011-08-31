c54d0a069e67299a43317a3626752cd745d376c7
Gerard Leijdekkers <gerard@eet.nu>
2011-08-31 21:41:07 +0200

    Added Quote#best_ma_period to look back and see what a decent period is to choose for your moving average

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -29,6 +29,28 @@ class Quote < ActiveRecord::Base
     (close - yesterday.exponential_moving_average) * multiplier + yesterday.exponential_moving_average
     
     previous_quotes(period).map(&attribute)
+  def best_ma_period(look_back_period, range_from = 5, range_to = 50)
+    max_value, max_period = 0, 0
+    previous = previous_quotes(look_back_period)
+    (range_from..range_to).each do |period|
+      stocks, value, cash = 0, 0, 10000
+      previous.each do |quote|
+        if (quote.close > quote.simple_moving_average(period)) && stocks == 0
+          stocks = (cash / quote.close).to_i
+          cash  -= (stocks * quote.close)
+        elsif (quote.close < quote.simple_moving_average(period)) && stocks > 0
+          cash += stocks * quote.close
+          stocks = 0
+        end
+      end
+      value = cash + stocks * self.close
+      if value > max_value
+        max_value  = value
+        max_period = period
+      end
+    end
+    puts "best period: #{max_period}"
+    max_period
   end
   
   private
