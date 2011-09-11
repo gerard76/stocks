b6847f602a3f3af38dcf5f68ea9c749d807cdc22
Gerard Leijdekkers <gerard@eet.nu>
2011-09-11 18:38:50 +0200

    cleanup

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -35,56 +35,6 @@ class Quote < ActiveRecord::Base
     StockMath.macd(previous_prices(long + 100), short, long)
   end
   
-  def best_sma_period(look_back_period, range_from = 5, range_to = 50)
-    max_value, max_period = 0, 0
-    previous = previous_quotes(look_back_period)
-    (range_from..range_to).each do |period|
-      stocks, value, cash = 0, 0, 10000
-      previous.each do |quote|
-        if (quote.close > quote.simple_moving_average(period)) && stocks == 0
-          stocks = (cash / quote.close).to_i
-          cash  -= (stocks * quote.close)
-        elsif (quote.close < quote.simple_moving_average(period)) && stocks > 0
-          cash += stocks * quote.close
-          stocks = 0
-        end
-      end
-      value = cash + stocks * self.close
-      if value > max_value
-        max_value  = value
-        max_period = period
-      end
-    end
-    puts "best period: #{max_period}"
-    max_period
-  end
-  memoize :best_sma_period
-  
-  def best_ema_period(look_back_period, range_from = 5, range_to = 50)
-    max_value, max_period = 0, 0
-    previous = previous_prices(look_back_period)
-    (range_from..range_to).each do |period|
-      stocks, value, cash = 0, 0, 10000
-      previous.each do |quote|
-        if (quote.close > quote.exponential_moving_average(period)) && stocks == 0
-          stocks = (cash / quote.close).to_i
-          cash  -= (stocks * quote.close)
-        elsif (quote.close < quote.exponential_moving_average(period)) && stocks > 0
-          cash += stocks * quote.close
-          stocks = 0
-        end
-      end
-      value = cash + stocks * self.close
-      if value > max_value
-        max_value  = value
-        max_period = period
-      end
-    end
-    puts "best period: #{max_period}"
-    max_period
-  end
-  memoize :best_ema_period
-  
   def previous_prices(period)
     count = Quote.where(symbol: symbol).where("date <= ?", date).count
     if count < period
--- lib/replay/sma_crossover_with_smooth.rb
+++ lib/replay/sma_crossover_with_smooth.rb
@@ -13,9 +13,9 @@ class SmaCrossoverWithSmooth < Replay
   def buy?
     sma_fast   = current_quote.sma(fast)
     sma_slow   = current_quote.sma(slow)
+    previous_quote = current_quote.previous_quotes(2).first
     
-    uptrend = sma_fast > current_quote.previous_quotes(2).first.sma(fast) &&
-                sma_slow > current_quote.previous_quotes(2).first.sma(slow)
+    uptrend = sma_fast > previous_quote.sma(fast) && sma_slow > previous_quote.sma(slow)
                 
     # only buy when the sma's cross and we're going up
     (sma_fast - sma_slow) > (smooth * current_quote.close) && stocks == 0
@@ -24,9 +24,9 @@ class SmaCrossoverWithSmooth < Replay
   def sell?
     sma_fast   = current_quote.sma(fast)
     sma_slow   = current_quote.sma(slow)
+    previous_quote = current_quote.previous_quotes(2).first
     
-    downtrend = sma_fast < current_quote.previous_quotes(2).first.sma(fast) &&
-                 sma_slow < current_quote.previous_quotes(2).first.sma(slow)
+    downtrend = sma_fast < previous_quote.sma(fast) && sma_slow < previous_quote.sma(slow)
     
     # sell fast if we're going down and the sma's cross
     if downtrend
