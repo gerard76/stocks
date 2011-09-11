4443872930c258cae455c82a1e51624dcbc2bcba
Gerard Leijdekkers <gerard@eet.nu>
2011-09-11 18:30:25 +0200

    Yield 11.27% per year

--- lib/replay/sma_crossover_with_smooth.rb
+++ lib/replay/sma_crossover_with_smooth.rb
@@ -13,15 +13,27 @@ class SmaCrossoverWithSmooth < Replay
   def buy?
     sma_fast   = current_quote.sma(fast)
     sma_slow   = current_quote.sma(slow)
-    # puts "diff: #{sma_fast - sma_slow} > #{(smooth * current_quote.close)}"
-    (sma_fast - sma_slow) > (smooth * current_quote.close) && stocks == 0
+    
+    uptrend = sma_fast > current_quote.previous_quotes(2).first.sma(fast) &&
+                sma_slow > current_quote.previous_quotes(2).first.sma(slow)
+                
+    # only buy when the sma's cross and we're going up
+    (sma_fast - sma_slow) > (smooth * current_quote.close) && uptrend && stocks == 0
   end
   
   def sell?
     sma_fast   = current_quote.sma(fast)
     sma_slow   = current_quote.sma(slow)
     
-    # sma_fast < sma_slow && stocks > 0
-    (sma_slow - sma_fast) > ((smooth/2) * current_quote.close) && stocks > 0
+    downtrend = sma_fast < current_quote.previous_quotes(2).first.sma(fast) &&
+                 sma_slow < current_quote.previous_quotes(2).first.sma(slow)
+    
+    # sell fast if we're going down and the sma's cross
+    if downtrend
+      (sma_fast < sma_slow) && stocks > 0
+    else
+      # wait a little to see what it does before selling
+      (sma_slow - sma_fast) > (smooth * current_quote.close) && stocks > 0
+    end
   end
 end
